b6ccaac854b05d6495479ea42988e3ce48ea6bac
¨isaias <isaiasrocha1626@gmail.com>
2024-05-06 22:04:49 -0300

    feat: add isOpen cart

--- app/restaurants/[id]/_components/cart-banner.tsx
+++ app/restaurants/[id]/_components/cart-banner.tsx
@@ -1,53 +1,64 @@
-"use client"
+    "use client";
 
-import Cart from "@/app/_components/cart";
-import { Button } from "@/app/_components/ui/button";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
-import { CartContext } from "@/app/_context/cart";
-import { formatCurrency } from "@/app/_helpers/price";
-import { Restaurant } from "@prisma/client";
-import { useContext } from "react";
+    import Cart from "@/app/_components/cart";
+    import { Button } from "@/app/_components/ui/button";
+    import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    } from "@/app/_components/ui/sheet";
+    import { CartContext } from "@/app/_context/cart";
+    import { formatCurrency } from "@/app/_helpers/price";
+    import { Restaurant } from "@prisma/client";
+    import { useContext, useState } from "react";
 
-interface CartBannerProp {
-    restaurant: Pick<Restaurant, 'id'>
-}
+    interface CartBannerProps {
+    restaurant: Pick<Restaurant, "id">;
+    }
 
-const CartBanner = ({restaurant}: CartBannerProp) => {
-    const { products, totalPrice, totalQuantity } = useContext(CartContext)
-    
-    const restaurantHasProductOnCart = products.filter((product) => product.restaurantId === restaurant.id)
+    const CartBanner = ({ restaurant }: CartBannerProps) => {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const { products, totalPrice, totalQuantity } = useContext(CartContext);
 
-    if (!restaurantHasProductOnCart) return null
+    const restaurantHasProductsOnCart = products.some(
+        (product) => product.restaurantId === restaurant.id,
+    );
 
-    return ( 
-        <div className="fixed bottom-0 left-0 p-5 pt-3 z-50 bg-white border-t shadow-md border-solid border-muted">
-            <div className="flex justify-between items-center">
-                <div>
-                    <span className="text-xs text-muted-foreground" >
-                        Total sem entrega
-                    </span>
-                    <h3 className="font-semibold">{formatCurrency(totalPrice)}
-                        <span className="text-xs text-muted-foreground font-normal"> /
-                            {totalQuantity} {totalQuantity > 1 ? "itens" : "item"}
-                        </span>
-                    </h3>
-                </div>
+    if (!restaurantHasProductsOnCart) return null;
 
-                <Sheet>
-                    <SheetTrigger>
-                        <Button className="font-semibold">Ver Sacola</Button>
-                    </SheetTrigger>
-                    <SheetContent className="w-[90vw]">
-                        <SheetHeader>
-                            <SheetTitle className="text-left">Sacola</SheetTitle>
-                        </SheetHeader>
-
-                        <Cart />
-                    </SheetContent>
-                </Sheet>
+    return (
+        <div className="fixed bottom-0 left-0 z-50 w-full border-t border-solid border-muted bg-white p-5 pt-3 shadow-md">
+        <div className="flex items-center justify-between">
+            {/* PREÇO */}
+            <div>
+            <span className="text-xs text-muted-foreground">
+                Total sem entrega
+            </span>
+            <h3 className="font-semibold">
+                {formatCurrency(totalPrice)}{" "}
+                <span className="text-xs font-normal text-muted-foreground">
+                {" "}
+                / {totalQuantity} {totalQuantity > 1 ? "itens" : "item"}
+                </span>
+            </h3>
             </div>
+            {/* BOTÃO */}
+
+            <Button onClick={() => setIsCartOpen(true)}>Ver sacola</Button>
+
+            <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
+            <SheetContent className="w-[90vw]">
+                <SheetHeader>
+                <SheetTitle className="text-left">Sacola</SheetTitle>
+                </SheetHeader>
+
+                <Cart setIsOpen={setIsCartOpen} />
+            </SheetContent>
+            </Sheet>
+        </div>
         </div>
     );
-}
+    };
 
-export default CartBanner;
+    export default CartBanner;
