b9e699a880d182f3476a92e1ab9c8e8edf85bfcc
¨isaias <isaiasrocha1626@gmail.com>
2024-05-06 22:35:16 -0300

    chore: add alt

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -1,14 +1,15 @@
 import Image, { ImageProps } from "next/image";
 
-    const PromoBanner = (props: ImageProps) => {
+const PromoBanner = (props: ImageProps) => {
+        
     return (
         <Image
-        height={0}
-        width={0}
-        className="h-auto w-full object-contain"
-        sizes="100%"
-        quality={100}
-        {...props}
+            height={0}
+            width={0}
+            className="h-auto w-full object-contain"
+            sizes="100%"
+            quality={100}
+            {...props}
         />
     );
     };
